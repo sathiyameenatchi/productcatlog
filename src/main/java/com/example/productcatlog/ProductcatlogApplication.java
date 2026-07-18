be4865501a0e17d8001c627ca85f2f6debb06480
kamathavan <mathucs@gmail.com>
2026-07-18 19:58:22 +0530

    this is project

---
+++ src/main/java/com/example/productcatlog/ProductcatlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.productcatlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcatlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductcatlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productcatlog/ProductcatlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productcatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
